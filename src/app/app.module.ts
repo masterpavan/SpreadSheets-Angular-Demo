b5b2d07a8987544dcdcf196f307c028a650eac74
Pavan Purewal <pavanpurewal0@gmail.com>
2018-09-15 03:10:43 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpreadSheetsModule } from '../lib/gc.spread.sheets.angular.11.2.1';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, SpreadSheetsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
